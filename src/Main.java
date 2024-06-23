54d09c83e643aa5182e96891b7edfaaba6f999f5
Pedro <pedroaraujo4910@gmail.com>
2024-06-23 14:59:43 -0300

    Subindo primeiro código Java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Soma: " + soma(1, 1));
+        System.out.println("Subtração: " + subtracao(1, 1));
+        System.out.println("Divisão: " + divicao(1, 1));
+        System.out.println("Multiplicação: " + multiplicacao(1, 1));
+    }
+
+    public static double soma(double num1, double num2) {
+        return num1 + num2;
+    }
+    public static double subtracao(double num1, double num2) {
+        return num1 - num2;
+    }
+    public static double divicao(double num1, double num2) {
+        return num1/num2;
+    }
+    public static double multiplicacao(double num1, double num2) {
+        return num1 * num2;
     }
 }
